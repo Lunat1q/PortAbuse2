bd8f87dea3cafc9dd371d1608e8697a560dd2414
Max <grimfire@gmail.com>
2017-09-16 22:45:24 +0300

    recheckin, missed files

---
+++ PortAbuse2.Core/Common/LabelNameAttribute.cs
@@ -0,0 +1,16 @@
+﻿using TiQWpfUtils.Controls.Extensions.DataGrid;
+
+namespace PortAbuse2.Core.Common
+{
+    public class LabelNameAttribute : LabelNameAttributeBase
+    {
+        public LabelNameAttribute(string label) : base(label)
+        {
+        }
+
+        public override string GetProperText()
+        {
+            return Label;
+        }
+    }
+}
---
+++ PortAbuse2.Core/WindowsFirewall/BlockMode.cs
@@ -0,0 +1,14 @@
+﻿using PortAbuse2.Core.Common;
+
+namespace PortAbuse2.Core.WindowsFirewall
+{
+    public enum BlockMode
+    {
+        [LabelName("Block all direction")]
+        BlockAll = 0,
+        [LabelName("Block input packets")]
+        BlockInput = 2,
+        [LabelName("Block output packets")]
+        BlockOutput = 4
+    }
+}
